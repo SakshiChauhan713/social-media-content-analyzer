84691dd55bddb2e6008249e9fa688ad27113b164
Sakshi Chauhan <2022.cse.2212699@gmail.com>
2025-08-31 16:37:33 +0530

    ✨ Final update: UI fixes + Dark/Light mode, History, Suggestions

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // 👈 add this line
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
